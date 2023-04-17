4dd026786de4dcb1a3466f3765e02ad19e0863e0
Jay Khimani <jay.khimani@gmail.com>
2023-04-17 02:19:37 +0200

    calling real api. If didnt find acocunt, returning dummy response for now

--- packages/snap/src/insights.ts
+++ packages/snap/src/insights.ts
@@ -1,4 +1,3 @@
-
 /**
  * As an example, get transaction insights by looking at the transaction data
  * and attempting to decode it.
@@ -9,6 +8,11 @@
 export async function getInsights(transaction: Record<string, unknown>) {
   try {
     console.log("In insights.ts");
+    const result = await getSimulationAssetChanges(transaction);
+    if(result.balance !== '') {
+      return result;
+    }
+    console.log("Could not find address from api call, returning dummy response");
     return { "balance": "3.12", "address": "0x72a5843cc08275c8171e582972aa4fda8c397b2a", "tags": "secondeye solution,EXPLOITER,US_GOV_BLOCKED", "firstTransactionTimestamp": "13-Dec-2017", "percentTransactionByRisk": ["40.64", "10.35", "50.11"], "riskScore": "10.00", "transactionTraces": { "topCreditsByRisk": [{ "id": "0", "address": "311f71389e3de68f7b2097ad02c6ad7b2dde4c71", "tags": "scam_phishing,CONTRACT,EXPLOITER", "directTransfer": { "amount": "0.00" }, "indirectTransfers": { "totalPaths": 1, "estimatedAmount": "2.10" } }], "topDebitsByRisk": [{ "id": "0", "address": "311f71389e3de68f7b2097ad02c6ad7b2dde4c71", "tags": "scam_phishing,CONTRACT,EXPLOITER", "directTransfer": { "amount": "0.00" }, "indirectTransfers": { "totalPaths": 1, "estimatedAmount": "1.10" } }] } };
   } catch (error) {
     console.error("----> ", error);
@@ -19,8 +23,12 @@ export async function getInsights(transaction: Record<string, unknown>) {
 }
 /* eslint-enable camelcase */
 
-// The API endpoint to get a list of functions by 4 byte signature.
-const API_ENDPOINT = 'https://api.demystify.network/address/score';
+// Change below as per the env. Figure out how to make this env specific (maybe use dotenv lib)
+const API_ENDPOINT = 'http://localhost:8443/address/score';
+// const API_ENDPOINT = 'https://api.demystify.network/address/score';
+
+// USE env spec. API key. 
+const API_KEY = "b98df9b5-8c3e-4e0a-be03-8aea2ee15f3a";
 
 /**
  * Gets the function name(s) for the given 4 byte signature.
@@ -32,16 +40,16 @@ const API_ENDPOINT = 'https://api.demystify.network/address/score';
  */
 async function getSimulationAssetChanges(
   transaction: Record<string, unknown>,
-): Promise<string[]> {
-  console.log("insights.ts: Inside getSimulationAssetChanges")
+) {
+  console.log("insights.ts: Inside getSimulationAssetChanges: ", transaction);
   const response = await fetch(API_ENDPOINT, {
     method: 'post',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({
-      "address": "0x72a5843cc08275C8171E582972Aa4fDa8C397B2A",
-      "apiKey": "B98DF9B5-8C3E-4E0A-BE03-8AEA2EE15F3A"
+      "address": transaction.to,
+      "apiKey": API_KEY
     }),
   });
 
@@ -51,8 +59,8 @@ async function getSimulationAssetChanges(
     throw new Error(errMsg);
   }
 
-  // The response is an array of objects, each with a "text_signature" property.
-  const { result } = (await response.json());
+  // // The response is an array of objects, each with a "text_signature" property.
+  const result =  await response.json();
   console.log("Result: ", result);
 
   return result;
