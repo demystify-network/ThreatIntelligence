52f176a8097c784e20f4ffbb5ce2d4b198912d12
Jay Khimani <jay.khimani@demystify.network>
2023-05-27 00:07:26 +0200

    DAPP: Change for the dapp

--- packages/site/src/utils/snap.ts
+++ packages/site/src/utils/snap.ts
@@ -50,14 +50,32 @@ export const getSnap = async (version?: string): Promise<Snap | undefined> => {
   }
 };
 
-/**
- * Invoke the "hello" method from the example snap.
- */
+export const sendContractTransaction = async (toAccount: string) => {
+  // Get the user's account from MetaMask.
+  const [from] = (await window.ethereum.request({
+    method: 'eth_requestAccounts',
+  })) as string[];
 
-export const sendHello = async () => {
+  enum TransactionConstants {
+    // Arbitrary contract that will revert any transactions that accidentally go through
+    Address = '0xa9b10d7c4548F4EDc24834e0A4F75Cb7b54D56A4',
+    // Some raw contract transaction data we will decode
+    UpdateWithdrawalAccount = '0x83ade3dc00000000000000000000000000000000000000000000000000000000000000400000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000000200000000000000000000000047170ceae335a9db7e96b72de630389669b334710000000000000000000000006b175474e89094c44da98b954eedeac495271d0f',
+    UpdateMigrationMode = '0x2e26065e0000000000000000000000000000000000000000000000000000000000000000',
+    UpdateCap = '0x85b2c14a00000000000000000000000047170ceae335a9db7e96b72de630389669b334710000000000000000000000000000000000000000000000000de0b6b3a7640000',
+  }
+
+  // Send a transaction to MetaMask.
   await window.ethereum.request({
-    method: 'wallet_invokeSnap',
-    params: { snapId: defaultSnapOrigin, request: { method: 'hello' } },
+    method: 'eth_sendTransaction',
+    params: [
+      {
+        from,
+        to: toAccount,
+        value: '0x0',
+        data: TransactionConstants.UpdateWithdrawalAccount,
+      },
+    ],
   });
 };
 
